f95eef426f15fa3dfec2725f5d2dbdc3454f6232
Abutalib Aghayev <agayev@psu.edu>
2021-01-28 16:16:30 -0500

    It is possible that we clean a not-full zone. In that case increase the number of free bytes by the number of bytes actually freed and not the zone size.
    Signed-off-by: Abutalib Aghayev <agayev@psu.edu>

--- src/os/bluestore/ZonedAllocator.cc
+++ src/os/bluestore/ZonedAllocator.cc
@@ -221,9 +221,9 @@ void ZonedAllocator::zoned_mark_zone_clean(uint64_t zone_num) {
   std::lock_guard l(lock);
   auto removed = cleaning_in_progress_zones.erase(zone_num);
   ceph_assert(removed);
+  num_free += zone_states[zone_num].write_pointer;
   zone_states[zone_num].num_dead_bytes = 0;
   zone_states[zone_num].write_pointer = 0;
-  num_free += zone_size;
 }
 
 void ZonedAllocator::zoned_set_zone_states(std::vector<zone_state_t> &&_zone_states) {
